3269c1eee11d7a0f36d9d1ec4a0d05c74e08f8d4
lfenart <lucas.fenart@protonmail.com>
2022-02-27 17:40:59 +0100

    Better matchmaking

--- src/utils/matchmaking.rs
+++ src/utils/matchmaking.rs
@@ -1,27 +1,24 @@
-use std::iter::FromIterator;
-
 use itertools::Itertools;
+use rand::Rng;
 use trueskill::{Rating, SimpleTrueSkill};
 
-#[inline]
-fn balance_internal(ratings: &[Rating]) -> Vec<usize> {
-    let goal = Rating::from_iter(ratings).mean() / 2.0 - ratings.last().unwrap().mean();
-    let len = ratings.len() - 2;
+fn balance_internal(players: &[f64]) -> u64 {
+    let goal = players.iter().sum::<f64>() / 2.0 - players[0];
+    let len = players.len();
     let mut best_score = f64::INFINITY;
-    let mut best_team1 = None;
-    for team1 in (0..len).combinations(len / 2) {
-        let score = team1
-            .iter()
-            .fold(goal, |acc, &x| acc - ratings[x].mean())
-            .abs();
+    let mut best_team1 = vec![];
+    for team1 in (1..len).combinations(len / 2 - 1) {
+        let score = (goal - team1.iter().map(|&x| players[x]).sum::<f64>()).abs();
         if score < best_score {
             best_score = score;
-            best_team1 = Some(team1);
+            best_team1 = team1;
         }
     }
-    let mut team1 = best_team1.unwrap();
-    team1.push(ratings.len() - 1);
-    team1
+    let mut bitmap = 1;
+    for x in best_team1 {
+        bitmap ^= 1 << x;
+    }
+    bitmap
 }
 
 pub fn balance<T: Copy>(players: &[(T, Rating)]) -> [Vec<(T, Rating)>; 2] {
@@ -30,16 +27,19 @@ pub fn balance<T: Copy>(players: &[(T, Rating)]) -> [Vec<(T, Rating)>; 2] {
         panic!("Not enough players");
     }
     let mut players = players.to_vec();
-    players.sort_by(|a, b| a.1.mean().partial_cmp(&b.1.mean()).unwrap());
-    let team1 = balance_internal(&players.iter().map(|x| x.1).collect::<Vec<_>>());
-    let mut index = 0;
+    players.sort_by(|a, b| b.1.mean().partial_cmp(&a.1.mean()).unwrap());
+    let team1 = balance_internal(&players.iter().map(|x| x.1.mean()).collect::<Vec<_>>());
+    let team1 = if rand::thread_rng().gen_bool(0.5) {
+        !team1
+    } else {
+        team1
+    };
     let mut teams = [
         Vec::with_capacity(len / 2),
         Vec::with_capacity((len + 1) / 2),
     ];
     for (i, player) in players.into_iter().enumerate() {
-        if team1[index] == i {
-            index += 1;
+        if team1 & (1 << i) != 0 {
             &mut teams[0]
         } else {
             &mut teams[1]
